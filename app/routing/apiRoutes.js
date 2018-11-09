4e3f235be1ab14c5a908fc2a9872474d60af04b2
Eric Guerrero <kteacherbecomescoder@gmail.com>
2018-11-08 17:53:39 -0700

    update

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,34 @@
+
+let surveyData = require("../data/survey");
+
+module.exports = function(app) {
+  // API GET Requests
+  app.get("/api/tables", function(req, res) {
+    res.json(tableData);
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    res.json(waitListData);
+  });
+
+  // API POST Requests
+  app.post("/api/tables", function(req, res) {
+    
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    tableData.length = [];
+    waitListData.length = [];
+
+    res.json({ ok: true });
+  });
+};
