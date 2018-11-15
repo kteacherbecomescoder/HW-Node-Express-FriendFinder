1a86527de5f59414a1f28cf7ce0c32532b6396cd
Eric Guerrero <kteacherbecomescoder@gmail.com>
2018-11-15 16:28:35 -0700

    commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,45 +1,57 @@
-
 var friends = require('../data/friends.js');
 
 module.exports = function (app) {
     app.get('/api/friends', function (req, res) {
         res.json(friends);
     });
-    
+
     app.post('/api/friends', function (req, res) {
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        };
+        var newFriend = req.body;
+        var bestMatch = {};
 
-        var userData = req.body;
-        var userScores = userData.scores;
-        
-        var userName = userData.name;
-        var userPhoto = userData.photo;
-
-        
-        var totalDifference = 0;
-
-        for (var i = 0; i < friends.length - 1; i++) {
-            console.log(friends[i].name);
-            totalDifference = 0;
-
-          
-            for (var j = 0; j < 10; j++) {
-                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-                if (totalDifference <= bestMatch.friendDifference) {
-                    bestMatch.name = friends[i].name;
-                    bestMatch.photo = friends[i].photo;
-                    bestMatch.friendDifference = totalDifference;
-                }
+        for (var i = 0; i < newFriend.scores.length; i++) {
+            if (newFriend.scores[i] == "1 (Strongly Disagree)") {
+                newFriend.scores[i] = 1;
+            } else if (newFriend.scores[i] == "5 (Strongly Agree)") {
+                newFriend.scores[i] = 5;
+            } else {
+                newFriend.scores[i] = parseInt(newFriend.scores[i]);
             }
         }
 
-        
-        friends.push(userData);
- 
+
+        var bestMatchIndex = 0;
+        var maxDifference = 40;
+
+
+        for (var i = 0; i < friends.length; i++) {
+            var totalDifference = 0;
+
+            for (var index = 0; index < friends[i].scores.length; index++) {
+                var differenceOneScore = Math.abs(friends[i].scores[index] - newFriend.scores[index]);
+                totalDifference += differenceOneScore;
+
+            }
+
+
+
+            if (totalDifference < maxDifference) {
+                bestMatchIndex = i;
+                maxDifference = totalDifference;
+            }
+        }
+
+
+        bestMatch = friends[bestMatchIndex];
+        friends.push(newFriend);
         res.json(bestMatch);
+
     });
+
+
+
+
+
+
+
 };
