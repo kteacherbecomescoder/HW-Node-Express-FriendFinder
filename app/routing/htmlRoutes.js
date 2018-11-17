80b93f8242f8eeeb408476c47d7f722694acaec5
Eric Guerrero <kteacherbecomescoder@gmail.com>
2018-11-16 17:46:07 -0700

    change

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
-  app.get("/survey.html", function (req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 };
